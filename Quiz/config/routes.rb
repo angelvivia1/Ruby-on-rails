5347440de1cb1f4a466f4edd8df4ca5fd3fec4ec
miguel <angel.vivia@gmail.com>
2016-06-15 23:25:52 -0500

    comitquiz
    quiz

---
+++ Quiz/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root 'questions#index'
+
+  resources :questions
+
+
+  get "quiz/index"
+  post "quiz/start"
+  get "quiz/question"
+  post "quiz/question"
+  post "quiz/answer"
+  get "quiz/end"
+  post "choices/create"
+  post "choices/destroy"
+
+  end
